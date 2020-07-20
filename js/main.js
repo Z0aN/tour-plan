ba7b6a25dc5f0849e842128ee4d6854b289b0f84
Z0aN <adrinal1ne30rus@mail.ru>
2020-07-20 20:27:45 +0400

    added: control from keyboard

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
-
-})
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
