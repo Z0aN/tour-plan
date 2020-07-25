7f1deaad15df88bc84d8c0ebfa3c609a6cd513a7
Z0aN <adrinal1ne30rus@mail.ru>
2020-07-25 12:31:48 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   keyboard: {
     enabled: true,
