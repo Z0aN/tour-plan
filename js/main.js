2d5795b1f5872a2dc346062acadb8bdb6edf57ca
Z0aN <adrinal1ne30rus@mail.ru>
2020-07-25 15:49:10 +0400

    copy: master

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
+  direction: "horizontal",
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [25.8251858, -80.23779831],
+    zoom: 10,
+  });
+}
