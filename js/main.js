7a823090e380d11f6c03c8ba8ce644a53705e093
Z0aN <adrinal1ne30rus@mail.ru>
2020-07-31 18:15:28 +0400

    create: modul window on Js

--- js/main.js
+++ js/main.js
@@ -1,52 +1,71 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-ymaps.ready(init);
-function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [25.8251858, -80.23779831],
-    zoom: 10,
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
   });
-  myMap.behaviors.disable("scrollZoom");
-  if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    //... отключаем перетаскивание карты
-    myMap.behaviors.disable("drag");
-  }
-}
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [25.8251858, -80.23779831],
+      zoom: 10,
+    });
+    myMap.behaviors.disable("scrollZoom");
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      //... отключаем перетаскивание карты
+      myMap.behaviors.disable("drag");
+    }
+  }
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
